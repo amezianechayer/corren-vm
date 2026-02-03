1ea63dc012f48e42b7b61eba304191131b45772a
amezianechayer <amezianechayer9@gmail.com>
2026-02-03 15:52:47 -0500

    Tests & debug errors

--- script/compiler/compiler.go
+++ script/compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/amezianechayer/aurex-vm/script/parser"
@@ -35,16 +36,56 @@ func (p *parseListener) ExitNumber(c *parser.NumberContext) {
 	p.program = append(p.program, program.OP_IPUSH, byte(a))
 }
 
-func Compile(input string) program.Program {
+type SyntaxError struct {
+	line, column int
+	msg          string
+}
+
+type CompileError []SyntaxError
+
+func (c *CompileError) Error() string {
+	s := ""
+	for _, e := range *c {
+		s = fmt.Sprintf("%v\nerror:%v:%v   %v", s, e.line, e.column, e.msg)
+	}
+	return s
+}
+
+type ErrorListener struct {
+	*antlr.DefaultErrorListener
+	Errors []SyntaxError
+}
+
+func (l *ErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	l.Errors = append(l.Errors, SyntaxError{
+		line:   line,
+		column: column,
+		msg:    msg,
+	})
+}
+
+func Compile(input string) (program.Program, error) {
 	listener := parseListener{
 		program: program.Program{},
 	}
 
+	elistener := &ErrorListener{}
+
 	is := antlr.NewInputStream(input)
 	lexer := parser.NewFaRlLexer(is)
+	lexer.RemoveErrorListeners()
+	lexer.AddErrorListener(elistener)
+
 	stream := antlr.NewCommonTokenStream(lexer, antlr.LexerDefaultTokenChannel)
 	p := parser.NewFaRlParser(stream)
+	p.RemoveErrorListeners()
+	p.AddErrorListener(elistener)
+
 	antlr.ParseTreeWalkerDefault.Walk(&listener, p.Script())
 
-	return listener.program
+	if len(elistener.Errors) != 0 {
+		return nil, (*CompileError)(&elistener.Errors)
+	}
+
+	return listener.program, nil
 }
